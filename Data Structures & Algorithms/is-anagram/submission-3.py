f79be930233a6c4c4754d64520b643db57bde209
Somya Dipayan <71426695+somyadipayan@users.noreply.github.com>
2026-04-16 08:11:55 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+            
+        hashmap = dict()
+
+        for i in s:
+            if i in hashmap:
+                hashmap[i]+=1
+            else:
+                hashmap[i] = 1
+
+        for i in t:
+            if i in hashmap:
+                hashmap[i]-=1
+
+        for i in hashmap.values():
+            if i != 0:
+                return False
+        return True
